f799af4271a63a12f512864bdf27d3b4c0e891ac
Oliver Middleton <olliemail27@gmail.com>
2014-10-24 20:25:04 +0100

    Kill datadata
    Change-Id: I4003a2f6e909675ade1f92652c66c00f88a053c1
    
    Conflicts:
    	BoardConfigCommon.mk
    	init.aries.rc

--- updater.sh
+++ updater.sh
@@ -5,8 +5,6 @@
 # Combined GSM & CDMA version
 #
 
-SYSTEM_SIZE='629145600' # 600M
-
 check_mount() {
     local MOUNT_POINT=`/tmp/busybox readlink $1`
     if ! /tmp/busybox test -n "$MOUNT_POINT" ; then
@@ -45,18 +43,6 @@ warn_repartition() {
     /tmp/busybox rm /.accept_wipe
 }
 
-format_partitions() {
-    /lvm/sbin/lvm lvcreate -L ${SYSTEM_SIZE}B -n system lvpool
-    /lvm/sbin/lvm lvcreate -l 100%FREE -n userdata lvpool
-
-    # format data (/system will be formatted by updater-script)
-    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -l -16384 -a /data /dev/lvpool/userdata
-
-    # unmount and format datadata
-    /tmp/busybox umount -l /datadata
-    /tmp/erase_image datadata
-}
-
 fix_package_location() {
     local PACKAGE_LOCATION=$1
     # Remove leading /mnt for Samsung recovery
@@ -86,13 +72,13 @@ if /tmp/busybox test "$1" = cdma ; then
     # CDMA mode
     IS_GSM='/tmp/busybox false'
     SD_PART='/dev/block/mmcblk1p1'
-    MMC_PART='/dev/block/mmcblk0p1 /dev/block/mmcblk0p2'
+    DATA_PART='/dev/block/mmcblk0p1'
     MTD_SIZE='490733568'
 else
     # GSM mode
     IS_GSM='/tmp/busybox true'
     SD_PART='/dev/block/mmcblk0p1'
-    MMC_PART='/dev/block/mmcblk0p2'
+    DATA_PART='/dev/block/mmcblk0p2'
     MTD_SIZE='442499072'
 fi
 
@@ -145,7 +131,7 @@ if /tmp/busybox test -e /dev/block/bml7 ; then
     exit 0
 
 elif /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE" || \
-    /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/name` != "datadata" ; then
+    /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/name` != "system" ; then
     # we're running on a mtd (old) device
 
     # make sure sdcard is mounted
@@ -163,10 +149,6 @@ elif /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE
 
     # inform the script that this is an old mtd upgrade
     /tmp/busybox echo 1 > /sdcard/cyanogenmod.mtdupd
-
-    # clear datadata
-    /tmp/busybox umount -l /datadata
-    /tmp/erase_image datadata
 
     # write new kernel to boot partition
     /tmp/bml_over_mtd.sh boot 72 reservoir 2004 /tmp/boot.img
@@ -193,14 +175,6 @@ elif /tmp/busybox test -e /dev/block/mtdblock0 ; then
     # unmount system and data (recovery seems to expect system to be unmounted)
     /tmp/busybox umount -l /system
     /tmp/busybox umount -l /data
-
-    # Resize partitions
-    # (For first install, this will get skipped because device doesn't exist)
-    if /tmp/busybox test `/tmp/busybox blockdev --getsize64 /dev/mapper/lvpool-system` -lt $SYSTEM_SIZE ; then
-        warn_repartition
-        /lvm/sbin/lvm lvremove -f lvpool
-        format_partitions
-    fi
 
     if $IS_GSM ; then
         # create mountpoint for radio partition
@@ -251,10 +225,8 @@ elif /tmp/busybox test -e /dev/block/mtdblock0 ; then
     # remove the cyanogenmod.cfg to prevent this from looping
     /tmp/busybox rm -f /sdcard/cyanogenmod.cfg
 
-    # setup lvm volumes
-    /lvm/sbin/lvm pvcreate $MMC_PART
-    /lvm/sbin/lvm vgcreate lvpool $MMC_PART
-    format_partitions
+    # format data (/system will be formatted by updater-script)
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -l -16384 -a /data $DATA_PART
 
     # restart into recovery so the user can install further packages before booting
     /tmp/busybox touch /cache/.startrecovery
